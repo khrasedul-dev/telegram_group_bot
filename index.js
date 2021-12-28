feb5f7c2378a65e2e5ad2c3cb7762ef9c7a2c301
khmehedihasan <87899184+khrasedul-dev@users.noreply.github.com>
2021-12-28 10:06:24 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // const { Telegraf } = require('telegraf')
-const {Composer} = require('micro-bot')
+const {Composer,Telegraf} = require('micro-bot')
 
 // const bot = new Telegraf("5026833569:AAHkz_WlSppeYYR-BgoAWOkheFjTQRPBJPc")
 
 const bot = new Composer()
 
 bot.on('new_chat_members', (ctx) => {
-    bot.telegram.sendMessage(ctx.chat.id,"Hello "+ctx.from.first_name + " "+ ctx.from.last_name +"\nHow can i help you?",{
+    Telegraf.telegram.sendMessage(ctx.chat.id,"Hello "+ctx.from.first_name + " "+ ctx.from.last_name +"\nHow can i help you?",{
         reply_markup:{
             inline_keyboard:[
                 [{text: "See all item", callback_data:"allItem"}]
@@ -18,7 +18,7 @@ bot.on('new_chat_members', (ctx) => {
 bot.action("allItem",(ctx)=>{
     ctx.answerCbQuery();
     ctx.deleteMessage();
-    bot.telegram.sendMessage(ctx.chat.id, "Choose an option",{
+    Telegraf.telegram.sendMessage(ctx.chat.id, "Choose an option",{
         reply_markup:{
             inline_keyboard:[
                 [{text: "Item 1",callback_data:"item1"}],
@@ -35,7 +35,7 @@ bot.action("allItem",(ctx)=>{
 bot.action("item1",(ctx)=>{
     ctx.answerCbQuery();
     ctx.deleteMessage();
-    bot.telegram.sendMessage(ctx.chat.id,"You are click item 1",{
+    Telegraf.telegram.sendMessage(ctx.chat.id,"You are click item 1",{
         reply_markup:{
             inline_keyboard:[
                 [{text: "Go Back", callback_data: "allItem"}]
